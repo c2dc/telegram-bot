b36e14e39f69749926fea821c59fd1a053499b84
Pedro Dias <pdcmpa@gmail.com>
2022-03-29 15:37:54 -0300

    Add base code to interact with Telethon

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ load_dotenv()
 
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
+client = TelegramClient('anon', api_id, api_hash)
+
+async def main():
+    me = await client.get_me()
+
+    print(me.stringify())
+
+with client:
+    client.loop.run_until_complete(main())
