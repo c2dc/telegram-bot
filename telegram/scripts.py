6e7f86dce5623b9c7947c4a1eb2c9bf92949374d
Pedro Dias <pdcmpa@gmail.com>
2022-11-08 20:31:59 -0300

    Change inactive-users to return tuple

--- telegram/scripts.py
+++ telegram/scripts.py
@@ -22,11 +22,18 @@ def activity_over_time(args, db: Database) -> None:
     df = df.groupby([df["message_utc"].dt.year, df["message_utc"].dt.month]).count()
 
     # Plot graph
-    df.plot(kind="bar")
+    df.plot(kind="bar", figsize=(15, 8.1))
+    plt.ylabel("Total de mensagens")
+    plt.xlabel("Mês/Ano")
+    plt.xticks(rotation=45)
+    plt.locator_params(axis="x", nbins=6)
+    plt.title("Atividade ao longo do tempo")
+    plt.legend().remove()
+    plt.savefig("activity_over_time.eps")
     plt.show()
 
 
-def inactive_users(args, db: Database) -> None:
+def inactive_users(args, db: Database) -> tuple:
     # Get full dialog
     dialog = db.get_channel_by_id(args.dialog_id)
 
@@ -34,7 +41,7 @@ def inactive_users(args, db: Database) -> None:
     df = pd.DataFrame(db.get_users_message_count(args.dialog_id))
     if df.shape[0] == 0:
         logger.info(f"Couldn't find users for dialog {dialog.name}")
-        return
+        return ()
 
     # If a user hasn't posted at least min_messages times, it's inactive
     inactive_users = df[df["count"] < args.min_messages]
@@ -46,3 +53,4 @@ def inactive_users(args, db: Database) -> None:
         f"{inactive_users.shape[0]/df.shape[0]:.2%} of the users in the dialog."
     )
     print(string)
+    return (inactive_users.shape[0], df.shape[0])
